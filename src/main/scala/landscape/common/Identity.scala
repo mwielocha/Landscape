c02f417e86f91b989910bf4fc62af72088a39057
Mikołaj Wielocha <mikolaj.wielocha@opigram.com>
2013-12-06 17:28:22 +0100

    Identity

---
+++ src/main/scala/landscape/common/Identity.scala
@@ -0,0 +1,34 @@
+package landscape.common
+
+import scala.Option
+
+/**
+ * author mikwie
+ *
+ */
+trait Equal[A] {
+
+  def isEqual(left: A, right: A): Boolean
+
+}
+
+trait Identity[T] {
+
+  def value: T
+
+  def ===(other: T)(implicit e: Equal[T]): Boolean = {
+    e.isEqual(value, other)
+  }
+  def !==(other: T)(implicit e: Equal[T]): Boolean = {
+    !(this === other)
+  }
+}
+
+object DefaultIdentityImplicits {
+
+  implicit def optionEqual[A] = new Equal[Option[A]] {
+    def isEqual(left: Option[A], right: Option[A]): Boolean = left == right
+  }
+
+  implicit class OptionIdentity[T](val value: Option[T]) extends Identity[Option[T]]
+}
